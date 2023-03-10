7f19e24bcda9c727a74d16f788ee2c35d7379d90
Ran LIU <rliu@e1r3p3.clusters.42paris.fr>
2023-03-10 07:25:44 +0100

    remove

--- Client.cpp
+++
@@ -1,7 +0,0 @@
-#include "Client.hpp"
-
-Client::Client(void) {}
-
-Client::~Client(void) {}
-
-Client::Client(const Client& src) { *this = src; }
--- Server.cpp
+++
@@ -1,7 +0,0 @@
-#include "Server.hpp"
-
-Server::Server(void) {}
-
-Server::~Server(void) {}
-
-Server::Server(const Server& src) { *this = src; }
