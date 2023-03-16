955729751d3d94cd712f7cc5f4dac9db5e3fbb1c
Ran LIU <55191863+RAN0606@users.noreply.github.com>
2023-03-16 12:35:44 +0100

    reorganiser finish

---
+++ bufferParser.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bufferParser.hpp                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rliu <rliu@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/14 15:42:23 by gduchate          #+#    #+#             */
+/*   Updated: 2023/03/16 11:56:08 by rliu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUFFERPARSER_HPP
+# define BUFFERPARSER_HPP
+# include "Client.hpp"
+# include <vector>
+# include <string>
+#include <iostream>
+# include <algorithm>
+# include "Message.hpp"
+# include "Client.hpp"
+# include "Command.hpp"
+
+std::vector<Message>  bufferParser(char* buf);
+void multiMessge_exec(std::vector<Message> msgList, Client& client);
+
+#endif
